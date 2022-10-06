1209819ed278b793ddd53be1bd8bad3f6355bf94
pablocavaz04 <camvazpablo04@gmail.com>
2022-10-06 10:34:28 +0200

    Horario a medias

---
+++ Ejercicio4.java
@@ -0,0 +1,18 @@
+// Ejercicio 4) Escribe un programa que muestre tu horario de clase.
+public class Ejercicio4 {
+    public static void main(String[] args){
+    System.out.println("               ┌──────────┬───────────────────────────────────────────────┐");
+    System.out.println("               │   LUNES  │  MARTES  │  MIÉRCOLES  │  JUEVES  │  VIERNES  │");
+    System.out.println("┌──────────────┼──────────┼──────────┼─────────────┼──────────┼───────────┤");
+    System.out.println("├ 8.15-9.15    ┼          ┼          ┼             ┼          ┼           ┤");
+    System.out.println("├──────────────┼ Program  ┼ Sistemas ┼  Entor.Desa ┼  Program ┼   F.O.L   ┤");
+    System.out.println("├ 9.15-10.15   ┼          ┼          ┼             ┼          ┼           ┤");
+    System.out.println("├──────────────┼──────────┼ Sistemas ┼─────────────┼──────────┼   F.O.L   ┤");
+    System.out.println("├ 10.15-11.15  ┼ Lenguaje ┼          ┼   Program   ┼  Program ┼           ┤");
+    System.out.println("├──────────────┼──────────┼──────────┼─────────────┼──────────┼───────────┤");
+    System.out.println("├         RECREO     RECREO      RECREO   RECREO     RECREO               ┤");
+    System.out.println("├──────────────┼──────────┼──────────┼─────────────┼──────────┼───────────┤");
+    System.out.println("");
+    }
+}
+
