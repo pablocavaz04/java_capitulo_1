772af341d2c75f6d9019734daabe38ccb91b5463
pablocavaz04 <camvazpablo04@gmail.com>
2022-10-06 08:53:40 +0200

    Fin

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,5 +1,5 @@
 public class Ejercicio1{
     public static void main(String[] args) {
-        System.out.print(" Pablo Camino Vázquez");
+        System.out.print("Pablito Camino Vázquez");
     }
 }
