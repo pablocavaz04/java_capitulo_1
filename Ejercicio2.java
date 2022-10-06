a2f1c023ea80c3fcdba59e443b76cfe173e8a185
pablocavaz04 <camvazpablo04@gmail.com>
2022-10-06 09:01:12 +0200

    Finalizado

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -1,7 +1,7 @@
 public class Ejercicio2 {
     public static void main(String[] args){
-        System.out.println("Pablo Camino Vázquez")
-        System.out.println("Pasillo de sierra blanca 14,1")
-        System.out.println("618381824)
+        System.out.println("Pablo Camino Vázquez");
+        System.out.println("Pasillo de sierra blanca 14,1");
+        System.out.println("618381824");
     }
 }
