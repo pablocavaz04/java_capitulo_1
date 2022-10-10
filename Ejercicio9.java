8821241797a7eca3e0a0dca25a7cff2bb1a3fbfc
pablocavaz04 <camvazpablo04@gmail.com>
2022-10-10 10:20:09 +0200

    Fin

---
+++ Ejercicio9.java
@@ -0,0 +1,8 @@
+public class Ejercicio9 {
+    public static void main(String[] args) {
+        System.out.println(" __|_");
+        System.out.println("|____|");
+        System.out.println("|    |");
+        System.out.println("|____|");
+    }
+}
