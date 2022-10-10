49c342963da36da78275e027e78d4d4f8e2d79c0
pablocavaz04 <camvazpablo04@gmail.com>
2022-10-10 10:23:10 +0200

    Fin

---
+++ Ejercicio10.java
@@ -0,0 +1,11 @@
+public class Ejercicio10 {
+        public static void main(String[] args) {
+            System.out.println("\033[31m");
+            System.out.println(" __|_");
+            System.out.println("|____|");
+            System.out.printf("\033[34m");
+            System.out.println("|    |");
+            System.out.println("|____|");
+    }
+}
+
