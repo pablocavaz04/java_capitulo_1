0384cff054e32dc03d94b6aad97f5e5b6b88b6e3
pablocavaz04 <camvazpablo04@gmail.com>
2022-10-10 09:14:12 +0200

    Fin

---
+++ Ejercicio6.java
@@ -0,0 +1,10 @@
+public class Ejercicio6 {
+// Escribe un programa que pinte por pantalla una pirámide rellena a base de asteriscos. La base de la pirámide debe estar formada por 9 asteriscos.
+    public static void main(String[] args){
+        System.out.println("    *");
+        System.out.println("   ***");
+        System.out.println("  *****");
+        System.out.println(" *******");
+        System.out.println("*********");
+    }
+}
