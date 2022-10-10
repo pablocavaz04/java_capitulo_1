f8dabcf3ee0788ded313ed10a73d3265614f5177
pablocavaz04 <camvazpablo04@gmail.com>
2022-10-10 09:08:35 +0200

    FIn

--- Ejercicio5.java
+++ Ejercicio5.java
@@ -3,19 +3,19 @@ public class Ejercicio5 {
     public static void main(String[] args){
     String blue = "\033[34m";
     String  orange = "\033[33m";
-    String purple = "033[35m";
-    
+    String purple = "\033[35m";
+    String red = "033[31m";
+
     System.out.println("               ┌──────────┬───────────────────────────────────────────────┐");
     System.out.println("               │   LUNES  │  MARTES  │  MIÉRCOLES  │  JUEVES  │  VIERNES  │");
     System.out.println("┌──────────────┼──────────┼──────────┼─────────────┼──────────┼───────────┤");
-    System.out.println("├ 8.15-9.15    ┼          ┼          ┼             ┼          ┼           ┤");
+    System.out.println(red+"├ 8.15-9.15    ┼          ┼          ┼             ┼          ┼           ┤");
     System.out.println("├──────────────┼ Program  ┼ Sistemas ┼  Entor.Desa ┼  Program ┼   F.O.L   ┤");
-    System.out.println("├ 9.15-10.15   ┼          ┼          ┼             ┼          ┼           ┤");
+    System.out.println(purple+"├ 9.15-10.15   ┼          ┼          ┼             ┼          ┼           ┤");
     System.out.println("├──────────────┼──────────┼ Sistemas ┼─────────────┼──────────┼   F.O.L   ┤");
-    System.out.println("├ 10.15-11.15  ┼ Lenguaje ┼          ┼   Program   ┼  Program ┼           ┤");
+    System.out.println(orange+"├ 10.15-11.15  ┼ Lenguaje ┼          ┼   Program   ┼  Program ┼           ┤");
     System.out.println("├──────────────┼──────────┼──────────┼─────────────┼──────────┼───────────┤");
-    System.out.println("├         RECREO     RECREO      RECREO   RECREO     RECREO               ┤");
+    System.out.println(blue+"├         RECREO     RECREO      RECREO   RECREO     RECREO               ┤");
     System.out.println("├──────────────┼──────────┼──────────┼─────────────┼──────────┼───────────┤");
-    System.out.println("");
     }
 }
