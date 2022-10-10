61b09d0efd1405b47ad17cd310f0e79acd499d44
pablocavaz04 <camvazpablo04@gmail.com>
2022-10-10 09:05:17 +0200

    Ejercicio 5 proceso

---
+++ Ejercicio5.java
@@ -0,0 +1,21 @@
+// Ejercicio 5) Escribe un programa que muestre tu horario de clase.
+public class Ejercicio5 {
+    public static void main(String[] args){
+    String blue = "\033[34m";
+    String  orange = "\033[33m";
+    String purple = "033[35m";
+    
+    System.out.println("               ┌──────────┬───────────────────────────────────────────────┐");
+    System.out.println("               │   LUNES  │  MARTES  │  MIÉRCOLES  │  JUEVES  │  VIERNES  │");
+    System.out.println("┌──────────────┼──────────┼──────────┼─────────────┼──────────┼───────────┤");
+    System.out.println("├ 8.15-9.15    ┼          ┼          ┼             ┼          ┼           ┤");
+    System.out.println("├──────────────┼ Program  ┼ Sistemas ┼  Entor.Desa ┼  Program ┼   F.O.L   ┤");
+    System.out.println("├ 9.15-10.15   ┼          ┼          ┼             ┼          ┼           ┤");
+    System.out.println("├──────────────┼──────────┼ Sistemas ┼─────────────┼──────────┼   F.O.L   ┤");
+    System.out.println("├ 10.15-11.15  ┼ Lenguaje ┼          ┼   Program   ┼  Program ┼           ┤");
+    System.out.println("├──────────────┼──────────┼──────────┼─────────────┼──────────┼───────────┤");
+    System.out.println("├         RECREO     RECREO      RECREO   RECREO     RECREO               ┤");
+    System.out.println("├──────────────┼──────────┼──────────┼─────────────┼──────────┼───────────┤");
+    System.out.println("");
+    }
+}
